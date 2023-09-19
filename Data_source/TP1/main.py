6008bc5d2ef91154315cbb27e8bf6d12b6b36acf
Marekepf <marek.boudeville@epfedu.fr>
2023-09-19 16:03:44 +0200

    FInal

---
+++ Data_source/TP1/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    prefix_google = """
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6JDE6HH266"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-6JDE6HH266');
+
+        </script>
+        """
+    return prefix_google + "Hello from Space! 🚀"
